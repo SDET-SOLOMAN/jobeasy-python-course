688178b8a0ad0b44eddcf859d318c215aa239931
sdet-soloman <sdetsoloman@gmail.com>
2022-09-17 12:49:09 -0500

    Done

--- lesson_4/homework_4_2.py
+++ lesson_4/homework_4_2.py
@@ -3,8 +3,11 @@
 # Ex. 1
 # Enter your name, save it in name variable and save in result_1 variable your name repeated 3 times (use loops)
 
-name_1 = None
-result_1 = None
+name_1 = "Sdet Soloman "
+result_1 = ""
+for num in range(3):
+    result_1 += name_1 * num
+print(result_1)
 
 # TODO: Here is your code
 
@@ -13,9 +16,14 @@ result_1 = None
 # Modify your previous program so that it will enter your name (save it in variable  name_2) and a number
 # (save in variable number) and then save in result_2 variable your name repeated as many times as number_1 is
 # (use loops)
-name_2 = None
-number_1 = None
-result_2 = None
+name_2 = input("Enter your name: ")
+number_1 = int(input("How many times to repeat? "))
+result_2 = ''
+
+for num in range(number_1):
+    result_2 += name_2 * num
+
+print(result_2)
 
 # TODO: Here is your code
 
@@ -24,15 +32,16 @@ result_2 = None
 # Enter a random string, which includes only digits. Write code which find a sum of digits in this string and save it
 # into result_3 variable
 
-string_number_1 = None
-result_3 = None
-
+string_number_1 = "12345678"
+result_3 = sum([int(x) for x in string_number_1])
+print(result_3)
 # TODO: Here is your code
 
 
 # Ex. 4
 # Create code which sums up all even numbers between 2 and 100 (include 100) and save it in result_4 variable
 
-result_4 = None
+result_4 = sum([x for x in range(2, 101) if x % 2 == 0])
+print(result_4)
 
 # TODO: Here is your code
--- lesson_4/homework_4_3.py
+++ lesson_4/homework_4_3.py
@@ -4,20 +4,38 @@
 # Write code, which will count how many times your character is included in your string.
 # Save result to result_1 variable
 
-string_1 = None
-char_1 = None
-result_1 = None
+string_1 = "Michael Jordan was a superstar, but not as good as Jordan Miohael"
+char_1 = 'a'
+result_1 = string_1.lower().count(char_1)
+print(result_1)
+
+#another way using while
+
+char_counter = 0
+index = 0
+while index < len(string_1):
+    if char_1 in string_1.lower()[index]:
+        char_counter += 1
+    index += 1
+print(char_counter)
 
 
 # Enter a random number and save it in variable number_1. Then create code to multiply all the digits together
 # and save result in the result_2 variable.
 
-number_1 = None
-result_2 = None
+number_1 = 123456789
+result_ = [x for x in str(number_1)]
+result_2 = 1
+
+for char in result_:
+    result_2 *= int(char)
+print(result_2)
+
 
 
 # Enter a random number and save it in variable number_2. Then create code which will return
 # a number with digits of number_1 in reverse order. Save it in result_3 variable
 
-number_2 = None
-result_3 = None
+number_2 = 8475632381001
+result_3 = int(str(number_2)[::-1])
+print(result_3)
